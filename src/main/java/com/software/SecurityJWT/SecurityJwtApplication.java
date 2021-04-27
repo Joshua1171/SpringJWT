84803989b7bc1ab032392460c95e616d69bd4987
Joshua1171 <59849366+Joshua1171@users.noreply.github.com>
2021-04-27 12:50:09 -0500

    agregando encriptacion de contraseña

--- src/main/java/com/software/SecurityJWT/SecurityJwtApplication.java
+++ src/main/java/com/software/SecurityJWT/SecurityJwtApplication.java
@@ -5,9 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SecurityJwtApplication {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SecurityJwtApplication.class, args);
 	}
-
+	String password = "12345";
+		
+		for(int i=0; i<2; i++) {
+			String bcryptPassword = passwordEncoder.encode(password);
+			System.out.println(bcryptPassword);
+		}
 }
